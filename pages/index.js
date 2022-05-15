73189ae817192ce9b6a350f3d75abe61b04542b6
Jonny McConnell <jonmccon@gmail.com>
2022-05-14 22:05:09 -0700

    get static props

--- pages/index.js
+++ pages/index.js
@@ -5,20 +5,18 @@ import Script from 'next/script'
 
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
+import { getSortedPostsData } from '../lib/posts';
 
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
 
-
-
-
-// example of threejs follow mouse
-// texture file may be on the mbp bu drive 
-// https://codepen.io/prisoner849/pen/yGMWNg
-// https://codepen.io/prisoner849/pen/XoRbNz
-
-
-
-
-export default function Home() {
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
@@ -31,9 +29,35 @@ export default function Home() {
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
       </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>List of md based posts</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
+      </section>
     </Layout>
   );
 }
+
+
+
+
+// example of threejs follow mouse
+// texture file may be on the mbp bu drive 
+// https://codepen.io/prisoner849/pen/yGMWNg
+// https://codepen.io/prisoner849/pen/XoRbNz
+
+
+
+
 
 
 
